feed3f1e5e7782e14b1adaa2cdaebfb17a7212cb
Euripedes Rocha <euripedes@quleap.com>
2020-05-03 19:09:53 +0100

    Updates tb with tbpp new interface.

--- tb/registers_tb.cpp
+++ tb/registers_tb.cpp
@@ -1,38 +1,41 @@
 #include <catch.hpp>
 #include <cstdint>
 #include <memory>
+#include <string>
 #include <tbpp.hpp>
 
 #include "Vregisters.h"
 
+namespace {
 enum class reg : uint8_t { zero, ra };
 
-class register_tb : public tbpp::base_clocked_tb<Vregisters, true> {
- public:
-  void write(reg reg_name, std::uint32_t data) {
-    dut->write_en = true;
-    dut->write_to = static_cast<uint8_t>(reg_name);
-    dut->write_data = data;
-    cycle();
-  }
-  auto read(reg reg_name) {
-    dut->read_from = static_cast<uint8_t>(reg_name);
-    cycle();
-    return dut->read_data;
-  }
-};
+using register_tb = tbpp::clocked_tb<Vregisters, true>;
+
+void write(register_tb& dut, reg reg_name, std::uint32_t data) {
+  dut->write_en = true;
+  dut->write_to = static_cast<uint8_t>(reg_name);
+  dut->write_data = data;
+  tbpp::run(dut);
+}
+auto read(register_tb& dut, reg reg_name) {
+  dut->read_from = static_cast<uint8_t>(reg_name);
+  tbpp::run(dut);
+  return dut->read_data;
+}
+
+}  // namespace
 
 TEST_CASE("Functional Verification of Registers", "[Reg]") {
   register_tb tb;
   SECTION("Write to R0 must not change it's value") {
-    tb.write(reg::zero, 5);
-    auto read = tb.read(reg::zero);
-    REQUIRE(read == 0);
+    write(tb, reg::zero, 5);
+    auto data = read(tb, reg::ra);
+    REQUIRE(data == 0);
   }
   SECTION("Write to any register") {
     tb.trace_to("registers.vcd");
-    tb.write(reg::ra, 5);
-    auto read = tb.read(reg::ra);
-    REQUIRE(read == 5);
+    write(tb, reg::ra, 5);
+    auto data = read(tb, reg::ra);
+    REQUIRE(data == 5);
   }
 }
